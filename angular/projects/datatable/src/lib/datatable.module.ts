fa2499e0f92221628a46f7917a90fe6ab6020e54
nikhilesh shinde <nikhilesh.shinde@wolterskluwer.com>
2020-04-08 17:45:36 +0530

    Restructure

---
+++ angular/projects/datatable/src/lib/datatable.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DatatableComponent } from './datatable.component';
+
+
+
+@NgModule({
+  declarations: [DatatableComponent],
+  imports: [
+  ],
+  exports: [DatatableComponent]
+})
+export class DatatableModule { }
